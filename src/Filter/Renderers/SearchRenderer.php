c7beac39da5943efec65d8144b0513feb205b878
Reza Esmaili <github@dfox.info>
2016-12-10 22:14:35 +0100

    Add fallback search method

--- src/Filter/Renderers/SearchRenderer.php
+++ src/Filter/Renderers/SearchRenderer.php
@@ -7,14 +7,38 @@ use Motor\Core\Filter\Base;
 class SearchRenderer extends Base
 {
 
+    protected $searchableColumns = [];
+
+
     public function render()
     {
         return view('motor-backend::filters.search', [ 'value' => $this->getValue() ]);
     }
 
 
+    public function setSearchableColumns($columns)
+    {
+        $this->searchableColumns = $columns;
+    }
+
+
     public function query($query)
     {
-        return $query->search($this->getValue());
+        if (method_exists($query, 'search')) {
+            return $query->search($this->getValue());
+        } else {
+            // Fallback solution in case the Eloquence trait is not available but we still want to search through the basic columns (see Navigation model in motor-cms for an example)
+            if (count($this->searchableColumns) == 0) {
+                return $query;
+            }
+            $searchableColumns = $this->searchableColumns;
+            $value             = $this->getValue();
+
+            return $query->orWhere(function ($query) use ($searchableColumns, $value) {
+                foreach ($searchableColumns as $column) {
+                    $query->where($column, 'LIKE', '%' . $value . '%');
+                }
+            });
+        }
     }
 }
