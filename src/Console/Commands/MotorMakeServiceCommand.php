7103436f416ca37450a79dc97ba2203de28d002d
Reza Esmaili <github@dfox.info>
2018-12-01 17:44:05 +0100

    Add more variables to generators

--- src/Console/Commands/MotorMakeServiceCommand.php
+++ src/Console/Commands/MotorMakeServiceCommand.php
@@ -26,15 +26,17 @@ class MotorMakeServiceCommand extends MotorMakeControllerCommand
 
     protected $type = 'Service';
 
+
     /**
      * Get the default namespace for the class.
      *
-     * @param  string  $rootNamespace
+     * @param  string $rootNamespace
+     *
      * @return string
      */
     protected function getDefaultNamespace($rootNamespace)
     {
-        return $rootNamespace.'\Services';
+        return $rootNamespace . '\Services';
     }
 
 
@@ -48,39 +50,38 @@ class MotorMakeServiceCommand extends MotorMakeControllerCommand
         if ($this->option('stub_path')) {
             return $this->option('stub_path');
         }
+
         return __DIR__ . '/stubs/service.stub';
     }
+
 
     /**
      * Replace the namespace for the given stub.
      *
-     * @param  string  $stub
-     * @param  string  $name
+     * @param  string $stub
+     * @param  string $name
+     *
      * @return $this
      */
     protected function replaceNamespace(&$stub, $name)
     {
         $class = last(explode('/', $this->getNameInput()));
 
-        $stub = str_replace(
-            'DummyRootNamespace', $this->getRootNamespace(), $stub
-        );
+        $stub = str_replace('DummyRootNamespace', $this->getRootNamespace(), $stub);
 
-        $stub = str_replace(
-            'DummyService', $class, $stub
-        );
+        $stub = str_replace('DummyService', $class, $stub);
 
-        $stub = str_replace(
-            'DummyModelPlural', Str::plural(str_replace('Service', '', $class)), $stub
-        );
+        $stub = str_replace('DummyModelPlural', Str::plural(str_replace('Service', '', $class)), $stub);
 
-        $stub = str_replace(
-            'DummyModel', Str::singular(str_replace('Service', '', $class)), $stub
-        );
+        $stub = str_replace('DummyModel', Str::singular(str_replace('Service', '', $class)), $stub);
 
-        $stub = str_replace(
-            'ComponentNameSingularLowercase', strtolower(Str::singular(str_replace('Component', '', str_replace('Service', '', $class)))), $stub
-        );
+        $stub = str_replace('ComponentNameSingularLowercase', strtolower(Str::singular(str_replace('Component', '', str_replace('Service', '', $class)))), $stub);
+
+        $stub = str_replace('ComponentNamePluralLowercase', strtolower(Str::plural(str_replace('Component', '', str_replace('Service', '', $class)))), $stub);
+
+        $stub = str_replace('ComponentNameSingularKebab', Str::kebab(Str::singular(str_replace('Component', '', str_replace('Service', '', $class)))), $stub);
+
+        $stub = str_replace('ComponentNamePluralKebab', Str::kebab(Str::plural(str_replace('Component', '', str_replace('Service', '', $class)))), $stub);
 
         return $this;
     }
