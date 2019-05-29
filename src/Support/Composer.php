3e225201085c9e3a245e9185b18c045cac9cfc28
Reza Esmaili <github@dfox.info>
2019-05-29 12:27:13 +0200

    Fix deprecation

--- src/Support/Composer.php
+++ src/Support/Composer.php
@@ -14,12 +14,8 @@ class Composer extends \Illuminate\Support\Composer
      */
     public function dumpAutoloads($extra = '')
     {
-        $process = $this->getProcess();
-
         $prefix = (env('MOTOR_PACKAGE_DEVELOPMENT') ? $prefix = 'COMPOSER=composer-dev.json ' : '');
-
-        $process->setCommandLine($prefix.trim($this->findComposer() . ' dump-autoload ' . $extra));
-
+        $process = $this->getProcess([$prefix.trim($this->findComposer() . ' dump-autoload ' . $extra)]);
         $process->run();
     }
 }
