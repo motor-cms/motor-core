3907d3e6932f6c0a2da2c9e9032fbb0e9978b643
Reza Esmaili <bitbucket@dfox.info>
2019-10-02 23:48:29 +0200

    Restore working generators

--- src/Console/Commands/MotorMakeMigrationCommand.php
+++ src/Console/Commands/MotorMakeMigrationCommand.php
@@ -18,11 +18,12 @@ class MotorMakeMigrationCommand extends MigrateMakeCommand
      *
      * @var string
      */
-    protected $signature = 'motor:make:migration {name : The name of the migration.}
-        {--create= : The table to be created.}
-        {--table= : The table to migrate.}
-        {--path= : The location where the migration file should be created.}
-        {--namespace= : The namespace for the migration}';
+    protected $signature = 'motor:make:migration {name : The name of the migration}
+        {--create= : The table to be created}
+        {--table= : The table to migrate}
+        {--path= : The location where the migration file should be created}
+        {--realpath : Indicate any provided migration file paths are pre-resolved absolute paths}
+        {--fullpath : Output the full path of the migration}';
 
 
     /**
--- src/Support/Composer.php
+++ src/Support/Composer.php
@@ -18,7 +18,7 @@ class Composer extends \Illuminate\Support\Composer
     public function dumpAutoloads($extra = ''): void
     {
         $prefix  = (env('MOTOR_PACKAGE_DEVELOPMENT') ? $prefix = 'COMPOSER=composer-dev.json ' : '');
-        $process = $this->getProcess([$prefix . trim($this->findComposer() . ' dump-autoload ' . $extra)]);
+        $process = $this->getProcess([$prefix . trim($this->findComposer()[0] . ' dump-autoload ' . $extra)]);
         $process->run();
     }
 }
