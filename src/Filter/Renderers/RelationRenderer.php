120115d8bd4abc569ca095aeffdcb0b1461f1c17
Reza Esmaili <bitbucket@dfox.info>
2023-12-04 17:18:13 +0100

    Fix relationRenderer with custom field names

--- src/Filter/Renderers/RelationRenderer.php
+++ src/Filter/Renderers/RelationRenderer.php
@@ -15,13 +15,26 @@ class RelationRenderer extends SelectRenderer
      */
     protected $options = null;
 
+    protected $relationField = null;
+
+    /**
+     * Base constructor.
+     */
+    public function __construct($name, $relationField = null)
+    {
+        $this->relationField = $relationField;
+        parent::__construct($name);
+    }
+
     /**
      * Run query for the filter
      */
     public function query(\Illuminate\Database\Eloquent\Builder|\Laravel\Scout\Builder $query): object
     {
+        $relationField = $this->relationField ?? Str::singular($query->getModel()->getTable()).'_id';
+
         if ($query instanceof Builder) {
-            return $query->join($this->join.' as '.$this->join, Str::singular($query->getModel()->getTable()).'_id', $query->getModel()->getTable().'.id')->where(
+            return $query->join($this->join.' as '.$this->join, $relationField, $query->getModel()->getTable().'.id')->where(
                 $this->join.'.'.$this->field,
                 $this->getValue()
             );
