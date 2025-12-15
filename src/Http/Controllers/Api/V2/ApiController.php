b87e6f950a3ee6f1051dc3eae1c5d050fce3f95f
Reza Esmaili <github@dfox.info>
2025-12-15 15:57:19 +0100

    feat(v2): add authorization and error handling to V2 ApiController
    - Add AuthorizesRequests trait for policy-based authorization
    - Add $model and $modelResource properties for authorizeResource()
    - Create V2ErrorHandler middleware for standardized error responses
    
    Part of EN-1812: API v2 Versioning

--- src/Http/Controllers/Api/V2/ApiController.php
+++ src/Http/Controllers/Api/V2/ApiController.php
@@ -2,13 +2,33 @@
 
 namespace Motor\Core\Http\Controllers\Api\V2;
 
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Http\Response;
 use Illuminate\Routing\Controller;
 use Motor\Core\Http\Traits\V2\HandlesApiErrors;
 
 abstract class ApiController extends Controller
 {
+    use AuthorizesRequests;
     use HandlesApiErrors;
+
+    /**
+     * The model class for authorization.
+     */
+    protected string $model = '';
+
+    /**
+     * The model resource name for authorization.
+     */
+    protected string $modelResource = '';
+
+    public function __construct()
+    {
+        if ($this->model && $this->modelResource) {
+            $this->authorizeResource($this->model, $this->modelResource);
+        }
+        \Locale::setDefault(config('app.locale'));
+    }
 
     /**
      * Return 204 No Content response for successful deletes.
