85d7aa3e1227e33411c73392c7c05e6910d50a29
Reza Esmaili <github@dfox.info>
2026-02-12 15:09:28 +0100

    Remove old code

--- src/Http/Resources/V2/BaseCollection.php
+++ src/Http/Resources/V2/BaseCollection.php
@@ -2,34 +2,9 @@
 
 namespace Motor\Core\Http\Resources\V2;
 
-use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
 abstract class BaseCollection extends ResourceCollection
 {
-    /**
-     * Customize pagination format for V2.
-     * Note: api_version is only added here (not in with()) to avoid duplication.
-     */
-    public function paginationInformation(Request $request, array $paginated, array $default): array
-    {
-        return [
-            'meta' => [
-                'api_version' => 'v2',
-                'pagination' => [
-                    'current_page' => $paginated['current_page'],
-                    'per_page' => $paginated['per_page'],
-                    'total' => $paginated['total'],
-                    'total_pages' => $paginated['last_page'],
-                    'has_more' => $paginated['current_page'] < $paginated['last_page'],
-                ],
-            ],
-            'links' => [
-                'first' => $paginated['first_page_url'],
-                'prev' => $paginated['prev_page_url'],
-                'next' => $paginated['next_page_url'],
-                'last' => $paginated['last_page_url'],
-            ],
-        ];
-    }
+    //
 }
