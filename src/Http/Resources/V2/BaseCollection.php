92b8d132593433620314537cf589835639b6315c
Reza Esmaili <github@dfox.info>
2026-02-17 00:58:16 +0100

    fix(v2): add api_version meta to BaseCollection responses
    BaseCollection was missing the with() method that BaseResource already
    had, causing all V2 collection endpoints to return null for
    meta.api_version. This fixes 42 failing V2 tests.
    
    Part of EN-1812: API v2 Refactoring

--- src/Http/Resources/V2/BaseCollection.php
+++ src/Http/Resources/V2/BaseCollection.php
@@ -2,9 +2,20 @@
 
 namespace Motor\Core\Http\Resources\V2;
 
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
 abstract class BaseCollection extends ResourceCollection
 {
-    //
+    /**
+     * Add api_version to all V2 collection responses.
+     */
+    public function with(Request $request): array
+    {
+        return [
+            'meta' => [
+                'api_version' => 'v2',
+            ],
+        ];
+    }
 }
