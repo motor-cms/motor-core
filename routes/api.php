ef22c8255a9eb914a675caf8b79893d7756f1ae3
Lukas Roth <mail@lukasroth.net>
2026-03-10 00:35:54 +0100

    fix: add v2 route name prefixes to prevent name collisions

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,8 @@ use Motor\Core\Http\Controllers\Api\V2\GlobalSearchController;
 
 Route::middleware(['auth:sanctum', \Motor\Core\Http\Middleware\V2\V2ErrorHandler::class])
     ->prefix('v2')
+    ->name('v2.')
     ->group(function () {
         Route::get('global-search', GlobalSearchController::class)
-            ->name('v2.global-search');
+            ->name('global-search');
     });
