a9a7d482682cf98682c8890e9ac87270b96da21e
Reza Esmaili <github@dfox.info>
2026-03-09 17:44:52 +0100

    fix: add V2ErrorHandler middleware to global-search route
    The global-search V2 route was missing the V2ErrorHandler middleware
    that all other V2 route groups include.
    
    Part of EN-1812: V2 API consistency audit fixes

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Motor\Core\Http\Controllers\Api\V2\GlobalSearchController;
 
-Route::middleware('auth:sanctum')
+Route::middleware(['auth:sanctum', \Motor\Core\Http\Middleware\V2\V2ErrorHandler::class])
     ->prefix('v2')
     ->group(function () {
         Route::get('global-search', GlobalSearchController::class)
